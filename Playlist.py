b45e84f30905be8b8532447b6837064e25609d2e
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-01-30 14:44:24 -0800

    worked on remove, add and length with Omar

--- Playlist.py
+++ Playlist.py
@@ -3,54 +3,77 @@ from Song import Song
 class Playlist:
   def __init__(self):
     self.__first_song = None
+    self.__count = 0
 
-
-  # TODO: Create a method called add_song that creates a Song object and adds it to the playlist. This method has one parameter called title.
+  #  Create a method called add_song that creates a Song object and adds it to the playlist. 
+  # This method has one parameter called title.
 
   def add_song(self, title):
-    new_song = Song(title)
-    new_song.__next_song = self.__first_song
-    self.__first_song = new_song 
+    #instantiate a new Song() class with title given
+    new_song = Song(title) 
+    new_song.set_next_song(self.__first_song) 
+    #print("next reference ", new_song.get_next_song())
+    self.__first_song = new_song
+    #print("head ", self.__first_song)
+    self.__count += 1
 
-
-  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
+  # TODO: Create a method called find_song that searches for whether a song exits in the playlist and returns its index. 
+  # The method has one parameters, title, which is the title of the song to be searched for. If the song is found, return its index.
 
   def find_song(self, title):
     current_index = 0
     current_song = self.__first_song
-    while current_song.__next_song:
-      current_song = current_song.__next_song
+    while current_song.get_next_song():
+      current_song = current_song.get_next_song()
       current_index += 1
-      if current_song.title == title:
+      if current_song.get_title() == title:
         return current_index
-      
 
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
+
+  # TODO: Create a method called remove_song that removes a song from the playlist. 
+  # This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
-    current = self.__first_song
-    previous = None
-    while current.__next_song is not None:
-      previous = current
-      current = current.__next_song
-    if previous is None:
-      self.head = current.__next_song
-    else:
-      previous.__next_song = current.__next_song
-    return previous
+    #current = self.__first_song
+    # previous = None
+    # while current.get_next_song() is not None:
+    #   previous = current
+    #   current = current.get_next_song()
+    # if previous is None:
+    #   self.head = current.get_next_song()
+    # else:
+    #   previous = current.get_next_song()
+    title = str(title).title()
+    song = self.__first_song
+    #base condition to check to see if the song is the head.
+    if title == song.get_title():
+      self.__first_song = song.get_next_song()
+      self.__count-= 1
+      #print(self.__first_song)
+      return 
+    while song.get_next_song() is not None:
+      next_song = song.get_next_song()
+     
+      if next_song.get_title() == title:
+        print(next_song)
+        #if songs next song is  = to the one we want to delete, set the songs, next song to next,next song
+        song.set_next_song(next_song.get_next_song())
+        self.__count -= 1
+        break
+      song = next_song
 
 
 
   # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
-    counter = 0 
-    current_song = self.__first_song
-    while current_song is not None:
-      counter = counter + 1
-      current_song = current_song.get_next_song
-    return counter
-
+    # counter = 0 
+    # current_song = self.__first_song
+    # while current_song is not None:
+    #   counter = counter + 1
+    #   current_song = current_song.get_next_song()
+    # return counter
+    return self.__count
 
   # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
 
@@ -65,3 +88,24 @@ class Playlist:
       print(current.get_title())
       current = current.get_next_song()
 
+if __name__ == "__main__":
+ playlist_one = Playlist()
+ print("============ADD SONGS ===================")
+ playlist_one.add_song("halo")
+ playlist_one.add_song("Sunrise")
+#  playlist_one.add_song("Tomorrow")
+#  playlist_one.add_song("Mountain")
+#  playlist_one.add_song("California")
+#  print('=============FIND SONG ======================')
+#  print(playlist_one.find_song("halo"))
+#  print('===============REMOVE SONG===================')
+#  print(playlist_one.remove_song("halo"))
+ print('====================PLAYLIST LENTGH==========')
+ print(playlist_one.length())
+ 
+ 
+ playlist_one.remove_song("halo")
+ print(playlist_one.length())
+ 
+#  print('====================PRINT SONGS================')
+#  print(playlist_one.print_songs())
--- Song.py
+++ Song.py
@@ -1,35 +1,52 @@
 class Song:
 
   def __init__(self, title):
-      self.__title = title
+      self.__title = str(title).title()
       self.__next_song = None
-
-
-  # TODO: Create a getter method for the title attribute, called get_title
-  def get_title(self):
-    return self.__title
   
-  
-  # TODO: Create a setter method for the next_song attribute, called set_title. Make sure titles are type cased to strings and are Title Cased.
-  def set_title(self, title):
-    self.__title =str(title)
-
-
-  # TODO: Create a getter method for the next_song attribute, called get_next_song
-  def get_next_song(self):
-    return self.__next_song
-
-
-  # TODO: Create a setter method for the next_song attribute, called set_next_song
-  def set_next_song(self, next_title):
-    self.__next_song = next_title
-
-
-  # TODO: Using the __str___ dunder method, return a string of the song title.
+  #Using the __str___ dunder method, return a string of the song title.
   def __str__(self):
     return f"{self.__title}"
 
 
-  # TODO: Using the __repr__ dunder method, return a string formatted as the following:'Song Title -> Next Song Title'
+  #  Using the __repr__ dunder method, return a string formatted as the following:
+  # 'Song Title -> Next Song Title'
   def __repr__(self):
-    return f' {self.__title} -> {self.__title.self__next_song.self.__title}'
+    return f'{self.__title} -> {self.__next_song}'
+
+
+  #Create a getter method for the title attribute, called get_title
+  def get_title(self):
+    return self.__title
+  
+  
+  # Create a setter method for the next_song attribute, called set_title. 
+  # Make sure titles are type cased to strings and are Title Cased.
+  def set_title(self, title):
+    self.__title = str(title).title()
+
+
+  #  Create a getter method for the next_song attribute, called get_next_song
+  def get_next_song(self):
+    return self.__next_song
+
+
+  #  Create a setter method for the next_song attribute, called set_next_song
+  def set_next_song(self, next_title):
+    self.__next_song = next_title
+
+
+  
+if __name__ == "__main__":
+    
+  song_one = Song("Halo")
+  print('=========SONG ONE============')
+  print(str(song_one))
+  # print('=================GET NEXT==============')
+  # print(song_one.get_next_song())
+  # print('===================SET NEXT SONG==============')
+  # print(song_one.set_next_song('California'))
+  # print('=============STR METHOD================')
+  # print(song_one.__str__())
+  # print('===========REPR===================')
+  # print(song_one.__repr__())
